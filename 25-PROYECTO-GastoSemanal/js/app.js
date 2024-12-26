aac247f275301b6df0c9b4211e45d9d963cc3f19
vera cristian <veracristianadrian@gmail.com>
2024-12-26 11:23:08 -0300

    avances app de gastos

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -1,7 +1,107 @@
 //variables y selectores
-
+const formulario = document.querySelector('#agregar-gasto');
+const gastoListado = document.querySelector('#gastos ul');
 
 //eventos
-
+eventListeners();
+function eventListeners(){
+    document.addEventListener('DOMContentLoaded', preguntarPresupuesto );
+    
+    formulario.addEventListener('submit',agregarGasto)
+}
 
 //clases 
+class Presupuesto{
+    constructor (presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos= [];
+    }
+
+    nuevoGasto(gasto){
+     this.gastos = [...this.gastos, gasto];   
+     console.log(this.gastos);
+    }
+}
+
+class UI{
+    insertarPresupuesto(cantidad){
+        //extrayendo valores
+        const {presupuesto, restante} = cantidad;
+
+        //insertando al html
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
+
+    imprimirAlerta(mensaje,tipo){
+        // crear el div
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');   // alert es una clase de boosttrap
+
+
+        if(tipo==='error'){
+            divMensaje.classList.add('alert-danger');  // le da un estilo de tipo danger
+        }else{
+            divMensaje.classList.add('alert-success');// le da un estilo de tipo correcto o verde
+        }
+        //mensaje de error 
+    divMensaje.textContent = mensaje;
+
+    // insertar en el html
+    document.querySelector('.primario').insertBefore(divMensaje, formulario);
+        //quitar del html 
+    setTimeout(() => {
+        divMensaje.remove();
+    }, 3000);
+    }
+    
+
+    
+
+}
+
+
+// variables globales
+const  ui = new UI();
+let presupuesto;
+//=======================================================================
+// funciones
+function preguntarPresupuesto(){
+    const presupuestoUsuario = prompt ('Cual es tu presupuesto?');
+
+    //console.log(Number(presupuestoUsuario));
+
+    if(presupuestoUsuario ==='' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0){
+        window.location.reload();
+    }
+    // Presupuesto valido 
+    presupuesto = new  Presupuesto(presupuestoUsuario);
+    console.log(presupuesto);
+    
+    ui.insertarPresupuesto(presupuesto) //llamamos al constructor de la clase con el objeto presup.. que ya tiene un  pres y restante
+    
+}
+//========================================================================
+//añade gastos
+
+function agregarGasto(e){
+    e.preventDefault();
+    //leer los datos del formulario
+    const nombre = document.querySelector('#gasto').value;
+    const cantidad = Number(document.querySelector('#cantidad').value);
+
+    //validar 
+    if (nombre===''|| cantidad===''){
+        ui.imprimirAlerta('Ambos campos son obligatorios','error');
+        return;
+    }else if(cantidad <=0 ||isNaN(cantidad) ){
+        ui.imprimirAlerta('Cantidad no valida','error');
+        return;
+    }
+    //generar un objeto con el gasto es de tipo llave valor pero como nombre y cantidad son el mismo nombre no hace falta la sintaxis
+    const gasto = { nombre, cantidad, id: Date.now() } 
+    // añade un nuevo gasto
+    presupuesto.nuevoGasto(gasto);
+    console.log(gasto);
+}
